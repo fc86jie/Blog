cda340adca7954601c41c61192a8541e8a874f13
fc86jie <wangrenjie86@gmail.com>
2023-01-08 12:19:06 +0800

    add css

--- docs/.vuepress/.temp/internal/pagesComponents.js
+++ docs/.vuepress/.temp/internal/pagesComponents.js
@@ -5,16 +5,20 @@ export const pagesComponents = {
   "v-8daa1a0e": defineAsyncComponent(() => import(/* webpackChunkName: "v-8daa1a0e" */"D:/project/Blog/docs/.vuepress/.temp/pages/index.html.vue")),
   // path: /css/base.html
   "v-531c1d43": defineAsyncComponent(() => import(/* webpackChunkName: "v-531c1d43" */"D:/project/Blog/docs/.vuepress/.temp/pages/css/base.html.vue")),
+  // path: /electron/base.html
+  "v-3b0d6374": defineAsyncComponent(() => import(/* webpackChunkName: "v-3b0d6374" */"D:/project/Blog/docs/.vuepress/.temp/pages/electron/base.html.vue")),
+  // path: /html/base.html
+  "v-e0c5db06": defineAsyncComponent(() => import(/* webpackChunkName: "v-e0c5db06" */"D:/project/Blog/docs/.vuepress/.temp/pages/html/base.html.vue")),
   // path: /javascript/base.html
   "v-17ed0aff": defineAsyncComponent(() => import(/* webpackChunkName: "v-17ed0aff" */"D:/project/Blog/docs/.vuepress/.temp/pages/javascript/base.html.vue")),
   // path: /javascript/browser.html
   "v-22e964fa": defineAsyncComponent(() => import(/* webpackChunkName: "v-22e964fa" */"D:/project/Blog/docs/.vuepress/.temp/pages/javascript/browser.html.vue")),
-  // path: /html/base.html
-  "v-e0c5db06": defineAsyncComponent(() => import(/* webpackChunkName: "v-e0c5db06" */"D:/project/Blog/docs/.vuepress/.temp/pages/html/base.html.vue")),
-  // path: /electron/base.html
-  "v-3b0d6374": defineAsyncComponent(() => import(/* webpackChunkName: "v-3b0d6374" */"D:/project/Blog/docs/.vuepress/.temp/pages/electron/base.html.vue")),
   // path: /miniprogram/base.html
   "v-ad652be6": defineAsyncComponent(() => import(/* webpackChunkName: "v-ad652be6" */"D:/project/Blog/docs/.vuepress/.temp/pages/miniprogram/base.html.vue")),
+  // path: /nodejs/link.html
+  "v-24bab858": defineAsyncComponent(() => import(/* webpackChunkName: "v-24bab858" */"D:/project/Blog/docs/.vuepress/.temp/pages/nodejs/link.html.vue")),
+  // path: /nodejs/other.html
+  "v-72b54898": defineAsyncComponent(() => import(/* webpackChunkName: "v-72b54898" */"D:/project/Blog/docs/.vuepress/.temp/pages/nodejs/other.html.vue")),
   // path: /npm/base.html
   "v-036777aa": defineAsyncComponent(() => import(/* webpackChunkName: "v-036777aa" */"D:/project/Blog/docs/.vuepress/.temp/pages/npm/base.html.vue")),
   // path: /react/base.html
@@ -23,10 +27,6 @@ export const pagesComponents = {
   "v-2e4c22b2": defineAsyncComponent(() => import(/* webpackChunkName: "v-2e4c22b2" */"D:/project/Blog/docs/.vuepress/.temp/pages/react/hooks.html.vue")),
   // path: /react/router.html
   "v-18e7fcc7": defineAsyncComponent(() => import(/* webpackChunkName: "v-18e7fcc7" */"D:/project/Blog/docs/.vuepress/.temp/pages/react/router.html.vue")),
-  // path: /nodejs/link.html
-  "v-24bab858": defineAsyncComponent(() => import(/* webpackChunkName: "v-24bab858" */"D:/project/Blog/docs/.vuepress/.temp/pages/nodejs/link.html.vue")),
-  // path: /nodejs/other.html
-  "v-72b54898": defineAsyncComponent(() => import(/* webpackChunkName: "v-72b54898" */"D:/project/Blog/docs/.vuepress/.temp/pages/nodejs/other.html.vue")),
   // path: /typescript/link.html
   "v-1ee8688e": defineAsyncComponent(() => import(/* webpackChunkName: "v-1ee8688e" */"D:/project/Blog/docs/.vuepress/.temp/pages/typescript/link.html.vue")),
   // path: /vue/other.html
--- docs/css/base.md
+++ docs/css/base.md
@@ -48,3 +48,91 @@
   </ul>
 </div>
 ```
+
+### fixed 定位失效
+
+由于 fixed 的基准元素发生了变化导致定位失效，有以下几种方式：
+
+1. transform 属性不为 none 的元素
+2. 设置了 transform-style:preserve-3d 的元素
+3. perspective 值不为 none 的元素
+4. 在 will-change 中指定了任意 css 属性
+5. 设置了 contain：paint
+6. filter 值不为 none 的元素
+7. backdrop-filter 值不为 none 的元素
+
+```html
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title></title>
+    <style type="text/css">
+      * {
+        padding: 0;
+        margin: 0;
+      }
+      #father {
+        width: 10vw;
+        height: 10vh;
+        background-color: red;
+        transform: translate3d(0, 0, 0);
+      }
+      #son {
+        position: fixed;
+        left: 1vw;
+        top: 1vw;
+        right: 1vw;
+        bottom: 1vw;
+        background-color: green;
+      }
+    </style>
+  </head>
+  <body>
+    <h3>transform不为none导致fixed失效</h3>
+    <div id="father">
+      <div id="son"></div>
+    </div>
+  </body>
+</html>
+```
+
+### 单行省略技巧
+
+```html
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title></title>
+    <style type="text/css">
+      * {
+        padding: 0;
+        margin: 0;
+      }
+      .container {
+        width: 200px;
+        height: 100px;
+        background-color: aqua;
+        overflow: hidden;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+      }
+      span {
+        color: white;
+        background-color: blueviolet;
+        border-radius: 4px;
+        /* display: inline-block; */
+      }
+    </style>
+  </head>
+  <body>
+    <h3>span的样式不加inline-block时会截断span中的文字显示，增加inline-block会省略整个span</h3>
+    <div class="container">
+      <span>前端工程师</span>
+      <span>JAVA工程师</span>
+      <span>GO工程师</span>
+    </div>
+  </body>
+</html>
+```
