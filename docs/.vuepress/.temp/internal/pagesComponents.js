36361100de3ca0eb9f3219ae0bf0c4d05981f5e5
fc86jie <wangrenjie86@gmail.com>
2023-01-15 13:41:14 +0800

    build

--- docs/.vuepress/.temp/internal/pagesComponents.js
+++ docs/.vuepress/.temp/internal/pagesComponents.js
@@ -3,14 +3,14 @@ import { defineAsyncComponent } from 'vue'
 export const pagesComponents = {
   // path: /
   "v-8daa1a0e": defineAsyncComponent(() => import(/* webpackChunkName: "v-8daa1a0e" */"D:/project/Blog/docs/.vuepress/.temp/pages/index.html.vue")),
-  // path: /browser/base.html
-  "v-6f859eb0": defineAsyncComponent(() => import(/* webpackChunkName: "v-6f859eb0" */"D:/project/Blog/docs/.vuepress/.temp/pages/browser/base.html.vue")),
   // path: /css/base.html
   "v-531c1d43": defineAsyncComponent(() => import(/* webpackChunkName: "v-531c1d43" */"D:/project/Blog/docs/.vuepress/.temp/pages/css/base.html.vue")),
-  // path: /html/base.html
-  "v-e0c5db06": defineAsyncComponent(() => import(/* webpackChunkName: "v-e0c5db06" */"D:/project/Blog/docs/.vuepress/.temp/pages/html/base.html.vue")),
+  // path: /browser/base.html
+  "v-6f859eb0": defineAsyncComponent(() => import(/* webpackChunkName: "v-6f859eb0" */"D:/project/Blog/docs/.vuepress/.temp/pages/browser/base.html.vue")),
   // path: /electron/base.html
   "v-3b0d6374": defineAsyncComponent(() => import(/* webpackChunkName: "v-3b0d6374" */"D:/project/Blog/docs/.vuepress/.temp/pages/electron/base.html.vue")),
+  // path: /html/base.html
+  "v-e0c5db06": defineAsyncComponent(() => import(/* webpackChunkName: "v-e0c5db06" */"D:/project/Blog/docs/.vuepress/.temp/pages/html/base.html.vue")),
   // path: /javascript/base.html
   "v-17ed0aff": defineAsyncComponent(() => import(/* webpackChunkName: "v-17ed0aff" */"D:/project/Blog/docs/.vuepress/.temp/pages/javascript/base.html.vue")),
   // path: /javascript/reg.html
@@ -29,10 +29,10 @@ export const pagesComponents = {
   "v-2e4c22b2": defineAsyncComponent(() => import(/* webpackChunkName: "v-2e4c22b2" */"D:/project/Blog/docs/.vuepress/.temp/pages/react/hooks.html.vue")),
   // path: /react/router.html
   "v-18e7fcc7": defineAsyncComponent(() => import(/* webpackChunkName: "v-18e7fcc7" */"D:/project/Blog/docs/.vuepress/.temp/pages/react/router.html.vue")),
-  // path: /vue/other.html
-  "v-afbd21aa": defineAsyncComponent(() => import(/* webpackChunkName: "v-afbd21aa" */"D:/project/Blog/docs/.vuepress/.temp/pages/vue/other.html.vue")),
   // path: /typescript/link.html
   "v-1ee8688e": defineAsyncComponent(() => import(/* webpackChunkName: "v-1ee8688e" */"D:/project/Blog/docs/.vuepress/.temp/pages/typescript/link.html.vue")),
+  // path: /vue/other.html
+  "v-afbd21aa": defineAsyncComponent(() => import(/* webpackChunkName: "v-afbd21aa" */"D:/project/Blog/docs/.vuepress/.temp/pages/vue/other.html.vue")),
   // path: /404.html
   "v-3706649a": defineAsyncComponent(() => import(/* webpackChunkName: "v-3706649a" */"D:/project/Blog/docs/.vuepress/.temp/pages/404.html.vue")),
 }
--- docs/.vuepress/.temp/internal/pagesRoutes.js
+++ docs/.vuepress/.temp/internal/pagesRoutes.js
@@ -1,9 +1,9 @@
 export const pagesRoutes = [
   ["v-8daa1a0e","/",{"title":""},["/index.html","/README.md"]],
-  ["v-6f859eb0","/browser/base.html",{"title":""},["/browser/base","/browser/base.md"]],
   ["v-531c1d43","/css/base.html",{"title":""},["/css/base","/css/base.md"]],
-  ["v-e0c5db06","/html/base.html",{"title":""},["/html/base","/html/base.md"]],
+  ["v-6f859eb0","/browser/base.html",{"title":""},["/browser/base","/browser/base.md"]],
   ["v-3b0d6374","/electron/base.html",{"title":""},["/electron/base","/electron/base.md"]],
+  ["v-e0c5db06","/html/base.html",{"title":""},["/html/base","/html/base.md"]],
   ["v-17ed0aff","/javascript/base.html",{"title":""},["/javascript/base","/javascript/base.md"]],
   ["v-5c12e78e","/javascript/reg.html",{"title":""},["/javascript/reg","/javascript/reg.md"]],
   ["v-ad652be6","/miniprogram/base.html",{"title":""},["/miniprogram/base","/miniprogram/base.md"]],
@@ -13,7 +13,7 @@ export const pagesRoutes = [
   ["v-33e27bff","/react/base.html",{"title":""},["/react/base","/react/base.md"]],
   ["v-2e4c22b2","/react/hooks.html",{"title":""},["/react/hooks","/react/hooks.md"]],
   ["v-18e7fcc7","/react/router.html",{"title":""},["/react/router","/react/router.md"]],
-  ["v-afbd21aa","/vue/other.html",{"title":""},["/vue/other","/vue/other.md"]],
   ["v-1ee8688e","/typescript/link.html",{"title":""},["/typescript/link","/typescript/link.md"]],
+  ["v-afbd21aa","/vue/other.html",{"title":""},["/vue/other","/vue/other.md"]],
   ["v-3706649a","/404.html",{"title":""},["/404"]],
 ]
